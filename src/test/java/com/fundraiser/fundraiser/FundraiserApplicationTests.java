fd79a0879ee63bd90560555415c74473c9cba87e
Beau0405 <beau.thompson81@gmail.com>
2024-05-03 16:30:53 -0500

    initial code

---
+++ src/test/java/com/fundraiser/fundraiser/FundraiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundraiser.fundraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
